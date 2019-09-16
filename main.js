659b0805d5c378c6ea65eb2cf01a5284f7039ce9
DESKTOP-485K4R8\mahesh <maheshevs@gmail.com>
2019-09-16 20:21:13 +0530

    Added dependencies and modified the business login as per the use cases provided

--- main.js
+++ main.js
@@ -1,28 +1,82 @@
-/**
- * http://usejsdoc.org/
- */
-
 https = require('https');
+var querystring = require('querystring');
+var buffers = [];
+var reviewData = [];
+
+var post_data = querystring.stringify({
+  'compilation_level' : 'ADVANCED_OPTIMIZATIONS',
+  'output_format': 'json',
+  'output_info': 'compiled_code',
+    'warning_level' : 'QUIET'
+});
+
 // options
 var options = {
    host: 'api.yelp.com',
-   path: '/v3/businesses/search?term=restorent&location=60007',
+   path: '/v3/businesses/search?term=restorent&location=60007&limit=5',
    methode:'GET',
-   headers:{'Authorization':'Bearer U_V_lSl8pcGuQ03WtqS0953YaeqOHnOgqUE2bfcOm1pIhJNB7q58_WoD8kWsIUO44l0r4TQGfyrP7w2oHdQFxEU4caijfHtPdSpkjo6VVxShFaP6p8DgupR-jo92XXYx'}
+   headers:{
+     'Authorization':'Bearer U_V_lSl8pcGuQ03WtqS0953YaeqOHnOgqUE2bfcOm1pIhJNB7q58_WoD8kWsIUO44l0r4TQGfyrP7w2oHdQFxEU4caijfHtPdSpkjo6VVxShFaP6p8DgupR-jo92XXYx',
+     'Content-Length': Buffer.byteLength(post_data)
+    }
 }
+// https://api.yelp.com/v3/businesses/{id}/reviews
+
 // get
 https.get(options, (res)=>{
    //console.log(res);
     res.on('data', (d) => {
-      // process.stdout.write(d);
-       console.log(JSON.parse(process.stdout.write(d)));
-      console.log(JSON.parse(process.stdout.write(d)));
+      buffers.push(d)
+     // console.log(d.toString());
     });
      res.on('end', (d) => {
-      // process.stdout.write(d);
-    //   console.log(d)
+    // console.log(JSON.parse(Buffer.concat(buffers).toString()));
+
+       var dataStr = JSON.parse(Buffer.concat(buffers).toString());
+       var business = dataStr.businesses;
+
+       business = business.sort(function(a, b){return b.review_count - a.review_count});// sorting with review count
+      // console.log(business);
+      //console.log(business);
+       for (let index = 0; index < business.length; index++) {
+         const element = business[index];
+        // console.log("element",element)
+         var reviewOption = {
+            host: 'api.yelp.com',
+            path: '/v3/businesses/'+ element.id + '/reviews',
+            methode:'GET',
+            headers:{
+              'Authorization':'Bearer U_V_lSl8pcGuQ03WtqS0953YaeqOHnOgqUE2bfcOm1pIhJNB7q58_WoD8kWsIUO44l0r4TQGfyrP7w2oHdQFxEU4caijfHtPdSpkjo6VVxShFaP6p8DgupR-jo92XXYx',
+              'Content-Length': Buffer.byteLength(post_data)
+            }
+         }
+         
+          https.get(reviewOption,(respRev) =>{
+            var reviewBufferData = [];
+            var reviewArr = [];
+             respRev.on('data', (d) => {reviewBufferData.push(d);});
+
+             respRev.on('end', (d) =>{
+              var reviews = JSON.parse(Buffer.concat(reviewBufferData).toString());
+              reviewData = reviews['reviews'];
+              reviewArr = [];
+            //  console.log('Reviews',reviewData)
+              reviewData.forEach(userDt => {
+               // console.log('userData:',userDt);
+                var review = {
+                  'Name Of Persiion:': userDt.user.name,
+                  'Review :' : userDt.text
+                }
+                reviewArr.push(review);
+              });
+             // console.log("ReviewArr",JSON.stringify(reviewArr));
+              console.log({'Business':element.name,'Address': element.location.address1 + ','+ element.location.city,'Review': JSON.stringify(reviewArr)});
+             }
+          )
+          respRev.on('error', (e) => {console.error(e);});
+            }
+          );
+       }
+      });
+          res.on('error', (e) => {console.error(e);});
     });
-   res.on('error', (e) => {
-     console.error(e);
-   });
-});
